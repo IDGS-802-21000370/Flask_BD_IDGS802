b620a04a431b763dee823ee4be3c68368788fb77
alejandroSalmeron <yahirmaster955@gmail.com>
2024-02-22 22:36:46 -0600

    YASM a 22 de febrero de 2024 Base de datos

---
+++ forms.py
@@ -0,0 +1,11 @@
+from wtforms import Form
+from wtforms import StringField, TextAreaField, SelectField, RadioField, IntegerField, EmailField
+from wtforms import validators
+
+class UserForm(Form):
+    nombre = StringField("nombre", [validators.DataRequired(message="el campo es requerido"), 
+                         validators.Length(min=4, max=10, message="ingresa nombre valido")])
+    email = EmailField("correo", [validators.Email(message="valor no valido")])
+    apaterno = StringField("apaterno")
+    edad=IntegerField('edad', [validators.number_range(min=1, max=20, message="valor no valido")])
+    
---
+++ main.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, render_template, Response
+import forms
+from flask_wtf.csrf import CSRFProtect
+from config import DevelopmentConfig
+from flask import flash
+from models import db
+app = Flask(__name__)
+app.config.from_object(DevelopmentConfig)
+csrf=CSRFProtect()
+
+@app.route("/")
+def index():
+    return render_template("layout2.html")
+
+
+@app.route("/alumnos", methods=["GET", "POST"])
+def alumnos():
+    nom=""
+    apaterno=""
+    correo=""
+    alum_form=forms.UserForm(request.form)
+    if request.method == "POST" and alum_form.validate():
+        nom = alum_form.nombre.data
+        correo = alum_form.email.data
+        apaterno = alum_form.apaterno.data
+        mensaje = "Bienvenido: {}".format(nom)
+        flash(mensaje)
+        print("nombre: {}".format(nom))
+        print("correo: {}".format(correo))
+        print("apaterno: {}".format(apaterno))
+    return render_template("alumnos.html", form=alum_form, nom=nom, apaterno=apaterno,correo=correo)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"),404
+
+if __name__ == "__main__":
+    csrf.init_app(app)
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+    app.run()
+
+
